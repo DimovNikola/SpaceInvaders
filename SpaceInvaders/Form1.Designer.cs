cf2735fe652e4fef771777f4737b5e096a0994b8
Nikola Dimov <nikoladimov45@yahoo.com>
2019-06-10 19:00:12 +0200

    Please enter the commit message for your changes. Lines starting

--- SpaceInvaders/Form1.Designer.cs
+++ SpaceInvaders/Form1.Designer.cs
@@ -28,21 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.accList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.insertAccName = new System.Windows.Forms.TextBox();
+            this.addAccount = new System.Windows.Forms.Button();
+            this.startGame = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.accList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 529);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // accList
+            // 
+            this.accList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.accList.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.accList.FormattingEnabled = true;
+            this.accList.Location = new System.Drawing.Point(3, 3);
+            this.accList.Name = "accList";
+            this.accList.Size = new System.Drawing.Size(323, 523);
+            this.accList.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.groupBox1.Controls.Add(this.startGame);
+            this.groupBox1.Controls.Add(this.addAccount);
+            this.groupBox1.Controls.Add(this.insertAccName);
+            this.groupBox1.Location = new System.Drawing.Point(332, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 523);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create/Select Account";
+            // 
+            // insertAccName
+            // 
+            this.insertAccName.Location = new System.Drawing.Point(69, 225);
+            this.insertAccName.Name = "insertAccName";
+            this.insertAccName.Size = new System.Drawing.Size(185, 20);
+            this.insertAccName.TabIndex = 0;
+            // 
+            // addAccount
+            // 
+            this.addAccount.BackColor = System.Drawing.Color.White;
+            this.addAccount.ForeColor = System.Drawing.Color.Navy;
+            this.addAccount.Location = new System.Drawing.Point(69, 263);
+            this.addAccount.Name = "addAccount";
+            this.addAccount.Size = new System.Drawing.Size(95, 23);
+            this.addAccount.TabIndex = 1;
+            this.addAccount.Text = "Create Account";
+            this.addAccount.UseVisualStyleBackColor = false;
+            // 
+            // startGame
+            // 
+            this.startGame.BackColor = System.Drawing.Color.White;
+            this.startGame.ForeColor = System.Drawing.Color.Navy;
+            this.startGame.Location = new System.Drawing.Point(161, 263);
+            this.startGame.Name = "startGame";
+            this.startGame.Size = new System.Drawing.Size(93, 23);
+            this.startGame.TabIndex = 2;
+            this.startGame.Text = "Play";
+            this.startGame.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1153, 606);
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(658, 529);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Space Invaders";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox accList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button startGame;
+        private System.Windows.Forms.Button addAccount;
+        private System.Windows.Forms.TextBox insertAccName;
     }
 }
 
