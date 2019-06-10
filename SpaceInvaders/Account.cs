c2d7952ccbba83098a3c7b284d9f4e0c22e7d1a3
Nikola Dimov <nikoladimov45@yahoo.com>
2019-06-10 19:07:39 +0200

    Created class Account

---
+++ SpaceInvaders/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    public class Account
+    {
+        private string name { get; set; }
+        private int score { get; set; }
+
+        public Account(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+    }
+}
